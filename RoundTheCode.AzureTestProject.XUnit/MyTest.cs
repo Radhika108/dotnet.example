4d73248bc307a40008c84223370a8f4d3f4c758e
rpather <radhika.pather@mecerdes-benz.co.za>
2023-03-29 10:21:47 +0200

    add code

---
+++ RoundTheCode.AzureTestProject.XUnit/MyTest.cs
@@ -0,0 +1,32 @@
+using RoundTheCode.AzureTestProject.Shared;
+using Xunit;
+
+namespace RoundTheCode.AzureTestProject.XUnit
+{
+    public class MyTest
+    {
+        [Fact]
+        public void CheckIsOK()
+        {
+            var myService = new MyService();
+
+            Assert.True(myService.IsOK());
+        }
+
+        [Fact]
+        public void CheckIsNotOK()
+        {
+            var myService = new MyService();
+
+            Assert.False(!myService.IsOK());
+        }
+
+        [Fact]
+        public void CheckIsNotNull()
+        {
+            var myService = new MyService();
+
+            Assert.True(!myService.Equals(null));
+        }
+    }
+}
